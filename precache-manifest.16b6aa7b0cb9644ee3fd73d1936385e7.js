c1d6168d3b4167826d31dead4f4e9406cbb0c98b
Arthur Silva <awls99@gmail.com>
2019-03-12 12:58:37 +0000

    Updates

---
+++ precache-manifest.16b6aa7b0cb9644ee3fd73d1936385e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81e8c340b36c21e75ec9",
+    "url": "/peoplesuckatdriving/static/css/main.45b05db2.chunk.css"
+  },
+  {
+    "revision": "81e8c340b36c21e75ec9",
+    "url": "/peoplesuckatdriving/static/js/main.9288e9b8.chunk.js"
+  },
+  {
+    "revision": "27348bfa336e47ab1934",
+    "url": "/peoplesuckatdriving/static/js/runtime~main.1f75d76d.js"
+  },
+  {
+    "revision": "ebee9e6ba35e5cd571ce",
+    "url": "/peoplesuckatdriving/static/js/2.59634610.chunk.js"
+  },
+  {
+    "revision": "87d1892c631d1645cab09189cce0495c",
+    "url": "/peoplesuckatdriving/index.html"
+  }
+];
